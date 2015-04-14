b5d27be495ad7b2625c1eed435aaf4f0bac8fd0f
oscar-vil <oscar.villegas@payson.se>
2015-04-14 10:16:38 +0200

    Upgradation
    Upgradation to PrestaShop 1.6.0.14

--- modules/paysondirect/validation.php
+++ modules/paysondirect/validation.php
@@ -1,7 +1,6 @@
 <?php
 include_once(dirname(__FILE__) . '/../../config/config.inc.php');
 include_once(dirname(__FILE__) . '/paysondirect.php');
-include_once(dirname(__FILE__) . '/../../header.php');
 
 if (version_compare(_PS_VERSION_, '1.5.0.0 ', '>=')) {
     $context = Context::getContext();
@@ -12,10 +11,6 @@ $token = $_GET["TOKEN"];
 $cart_id = intval($_GET["id_cart"]);
 
 $payson = new Paysondirect();
-
-if ($cart->id_customer == 0 OR $cart->id_address_delivery == 0 OR $cart->id_address_invoice == 0 OR !$payson->active)
-    Tools::redirectLink(__PS_BASE_URI__ . 'order.php?step=1');
-
 
 // Check that this payment option is still available in case the customer changed his address just before the end of the checkout process
 $authorized = false;
